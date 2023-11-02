3018e99856c3675e43d76d895ec9acb8d2e557a9
Oscar Karbin <karbin.oscar2002@gmail.com>
2023-11-02 14:11:52 +0000

    Exercises almost done.

--- HASKELL/Exercises/exercises_1.hs
+++ HASKELL/Exercises/exercises_1.hs
@@ -17,9 +17,6 @@ cakeSplit n | n > 150   = 1 + cakeSplit(n-100)
 
 --Problem 2: Weekly sales
 sales :: Int -> Integer
-sales 1 = 30
-sales 2 = 70
-sales 3 = 2000
 sales i = randomRs (0,1000) (mkStdGen i) !! i
 
 
@@ -41,3 +38,32 @@ highestSales n = biggerOrLower n 0 0 -- Weeks, currentWeek, Highets
                               | otherwise = s2
 -----------------------------------------------------
 
+--C---------------------------------------------
+badPerformace :: Int -> Integer
+badPerformace 0 = 0
+badPerformace n | sales(n) < 100 = 1 + badPerformace(n-1)
+                | otherwise      = badPerformace(n-1)
+------------------------------------------------
+
+--D-------------------------------------------------
+
+totalSales2 :: Int -> Integer 
+totalSales2 n = sum [sales i | i <- [0..n]]
+
+highestSales2 :: Int -> Integer
+highestSales2 n = maximum [sales i | i <- [0..n]]
+
+badPerformace2 :: Int -> Int
+badPerformace2 n = length [ i | i <- [0..n], sales i < 100]
+
+-------------------------------------------------------
+
+--E- Average sales up to and including week n?-------
+
+avgSales :: Int -> Integer
+avgSales 0 = 0
+avgSales n = (totalSales2 n) `div` fromIntegral n
+
+------------------------------------
+
+--F Give the week numbers of the weeks with the best sales in the first n weeks. Since there may be more than one week with the best sales, your answer should be a list of weeks.----
