904aa89965cd69c1700dbdb1a073f99cf97756ad
oscarkarbin <karbin.oscar2002@gmail.com>
2023-12-13 12:35:21 +0000

    lab4 done.

--- HASKELL/Lab4/Calculator.hs
+++ HASKELL/Lab4/Calculator.hs
@@ -6,34 +6,31 @@ import ThreepennyPages
 import Graphics.UI.Threepenny.Core as UI
 import qualified Graphics.UI.Threepenny as UI
 
+
 canWidth,canHeight :: Num a => a
 canWidth  = 300
 canHeight = 300
 
-points :: Expr -> Double -> (Int, Int) -> [Point]
-points exp scale (width, height) =
-  [ (fromIntegral x, fromIntegral y)
-    | x <- [0 .. width - 1]
-    , let val = eval exp ((fromIntegral x - fromIntegral (width `div` 2)) / scale)
-          y = height - round (val * scale + fromIntegral (height `div` 2))
-  ]
 
 main :: IO ()
 main = startGUI defaultConfig setup
 
 setup :: Window -> UI ()
-setup window =
-  do -- Create them user interface elements
+setup window = do -- Create them user interface elements
      canvas  <- mkCanvas canWidth canHeight   -- The drawing area
      fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
      input   <- mkInput 20 "x"                -- The formula input
      draw    <- mkButton "Draw graph"         -- The draw button
+     zoomSlider <- mkSlider (1, 100) 20       -- Add a slider for zooming
+     diffButton <- mkButton "Differentiate"   -- Create a Differentiate button
+    
+    
        -- The markup "<i>...</i>" means that the text inside should be rendered
        -- in italics.
 
      -- Add the user interface elements to the page, creating a specific layout
      formula <- row [pure fx,pure input]
-     getBody window #+ [column [pure canvas,pure formula,pure draw]]
+     getBody window #+ [column [pure canvas,pure formula,pure draw, pure zoomSlider, pure diffButton]]
 
      -- Styling
      getBody window # set style [("backgroundColor","lightblue"),
@@ -41,36 +38,55 @@ setup window =
      pure input # set style [("fontSize","14pt")]
 
      -- Interaction (install event handlers)
-     on UI.click     draw  $ \ _ -> readAndDraw input canvas
-     on valueChange' input $ \ _ -> readAndDraw input canvas
+     on UI.click     draw  $ \ _ -> readAndDraw input canvas zoomSlider
+     on valueChange' input $ \ _ -> readAndDraw input canvas zoomSlider
+     on valueChange' zoomSlider $ \_ -> readAndDraw input canvas zoomSlider
+     on UI.click diffButton $ \_ -> differentiateAndDraw input canvas zoomSlider
 
+-- readAndDraw takes an input field, a canvas, and a zoom control element.
+-- If the expression is valid, it draws a path representing the expression with the given zoom factor.
+readAndDraw :: Element -> Canvas  -> Element -> UI ()
+readAndDraw input canvas zoom = do
+    formula <- get value input 
+    zoomFactor <- get value zoom
 
--- readAndDraw :: Element -> Canvas -> UI ()
--- readAndDraw input canvas =
---   do -- Get the current formula (a String) from the input element
---      formula <- get value input
---      -- Clear the canvas
---      clearCanvas canvas
---      -- The following code draws the formula text in the canvas and a blue line.
---      -- It should be replaced with code that draws the graph of the function.
---      set UI.fillStyle (UI.solidColor (UI.RGB 0 0 0)) (pure canvas)
---      UI.fillText formula (10,canHeight/2) canvas
---      path "blue" [(10,10),(canWidth-10,canHeight/2)] canvas
-
--- Function to read and draw expression graph
-readAndDraw :: Element -> Canvas -> UI ()
-readAndDraw input canvas = do
-    formula <- get value input
-    clearCanvas canvas
     case readExpr formula of
-        Just expr -> drawGraph canvas expr
-        Nothing   -> return ()  -- or handle error
+      Just expr -> do
+        clearCanvas canvas
+        let scale = read zoomFactor  
+            pointList = points expr scale (canWidth, canHeight)
+        path "blue" pointList canvas
 
--- Function to draw the graph of an expression
-drawGraph :: Canvas -> Expr -> UI ()
-drawGraph canvas expr = do
-    let scale = 20  -- Adjust scale as necessary
-    let graphPoints = points expr scale (canWidth, canHeight)
-    path "red" graphPoints canvas
+      Nothing   -> return ()
 
 
+-- differentiateAndDraw takes an input field, a canvas, and a zoom control slider. It updates the
+-- input field with the differentiated expression, and draws the new expression on
+-- the canvas with the specified zoom factor.
+differentiateAndDraw :: Element -> Canvas -> Element -> UI ()
+differentiateAndDraw input canvas zoomSlider = do
+    formula <- get value input
+    zoomFactor <- get value zoomSlider
+
+    case readExpr formula of
+      Just expr -> do
+        let diffExpr = differentiate expr
+            scale = read zoomFactor
+            pointList = points diffExpr scale (canWidth, canHeight)
+
+        element input # set value (showExpr diffExpr)
+        clearCanvas canvas
+        path "blue" pointList canvas
+
+      Nothing   -> return ()
+
+-- | 'points' takes an expression, a scaling factor, and canvas dimensions (width, height).
+-- It generates a list of points representing the expression evaluated at each x-coordinate
+-- across the canvas, scaled and positioned according to the given dimensions and scale.
+points :: Expr -> Double -> (Int, Int) -> [Point]
+points exp scale (width, height) =
+  [ (fromIntegral x, fromIntegral y)
+    | x <- [0 .. width - 1]
+    , let val = eval exp ((fromIntegral x - fromIntegral (width `div` 2)) / scale)
+          y = height - round (val * scale + fromIntegral (height `div` 2))
+  ]
--- HASKELL/Lab4/Expr.hs
+++ HASKELL/Lab4/Expr.hs
@@ -1,12 +1,12 @@
 module Expr where
 
-
 import Parsing
 import Data.Char (isSpace)
 import Test.QuickCheck
 
-
--- A --------------------------------------------------
+-- Definition of the Expr data type and related types
+-- 'Expr' represents a mathematical expression which can be a number, 
+-- an operation (addition or multiplication), a variable 'X', or a function (sin or cos).
 data Expr
     = Num Double
     | AddMul Opp Expr Expr
@@ -14,79 +14,60 @@ data Expr
     | Func SinCos Expr
     deriving (Eq, Show)
 
+-- 'SinCos' defines trigonometric functions: sine and cosine.
 data SinCos = Sin | Cos
     deriving (Eq, Show)
 
+-- 'Opp' defines binary operations: addition and multiplication.
 data Opp = Add | Mul
     deriving (Eq, Show)
 
--- instance Show Expr where
---     show = showExpr -- this is needed so we can print nicely by simply typing "ex1 " in the console
-
+-- Helper functions to create expressions
 x :: Expr
 x = X
 
 num :: Double -> Expr
-num n = Num n
+num = Num
 
 add,mul :: Expr -> Expr -> Expr
-add e1 e2 = AddMul Add e1 e2
-mul e1 e2 = AddMul Mul e1 e2
+add = AddMul Add
+mul = AddMul Mul
 
 sin,cos :: Expr -> Expr
-sin e = Func Sin e
+sin = Func Sin
 
-cos e = Func Cos e
+cos = Func Cos
 
--- (1+sin(2)) * x
+-- Example expression
 ex1 = AddMul Mul (AddMul Add (Num 1.0) (Func Sin (Num 2))) X
 
-
+-- Function to calculate the size of an expression
 size :: Expr -> Int
 size (Num n)          = 0
 size X                = 0
 size (AddMul _ e1 e2) = 1 + (size e1 + size e2)
 size (Func _ e1)      = 1 + size e1
--- END A --------------------------------------------------------------------------
 
-
--- B ------------------------------------------------------------------------------
-
-{-
-When showing and reading expressions, we have to decide where we allow and require parentheses. 
-Parentheses are required only in the following cases:
-
-When the arguments of a *-expression use +. For example: (3.1+4.2)*7
-When the argument of sin or cos uses * or +. For example: sin (3.2*x)
-
-data Expr
-    = Num Double
-    | AddMul String Expr Expr
-    | X
-    | Func String Expr
-    deriving (Eq, Show)
--}
-
+-- Function to convert an expression to a string
 showExpr :: Expr -> String
 showExpr (Num n)            = show n
 showExpr (AddMul Add e1 e2) = showExpr e1 ++ "+" ++ showExpr e2
 showExpr (AddMul Mul e1 e2) = showFactor e1 ++ "*" ++ showFactor e2
-showExpr (Func Sin e1)    = "sin" ++ showFunc e1
-showExpr (Func Cos e1)    = "cos" ++ showFunc e1
+showExpr (Func Sin e1)    = "sin " ++ showFunc e1
+showExpr (Func Cos e1)    = "cos " ++ showFunc e1
 showExpr X                  = "x"
 
+-- Function to convert an expression to a string
 showFactor :: Expr -> String
 showFactor (AddMul Add e1 e2) = "(" ++ showExpr (AddMul Add e1 e2) ++ ")"
 showFactor e                  = showExpr e
 
+-- Function to convert an expression to a string
 showFunc :: Expr -> String
 showFunc (AddMul op e1 e2) = "(" ++ showExpr (AddMul op e1 e2) ++ ")"
 showFunc e                = showExpr e
 
--- END B----------------------------------------------------------------
-
--- C ---------------------------------------------------------------------
-
+-- Function to evaluate an expression with a given value for 'X'
 eval :: Expr -> Double -> Double
 eval (Num n) x            = n
 eval X x                  = x
@@ -95,12 +76,7 @@ eval (AddMul Mul e1 e2) x = eval e1 x * eval e2 x
 eval (Func Sin e1) x      = Prelude.sin (eval e1 x)
 eval (Func Cos e1) x      = Prelude.cos (eval e1 x)
 
-
--------------------------------------------------------------------------------
-
--- D -------------------------------------------------------------------
-
-
+-- Parsers for different components of an expression
 number :: Parser Expr
 number = do
     d <- readsP :: Parser Double
@@ -127,22 +103,20 @@ string s = filter (not . isSpace) <$> traverse char s
 
 func :: Parser Expr
 func = do
-    f <- string "sin" <|> string "cos"
+    f <- string "sin " <|> string "cos "
     e <- factor
     return $ case f of
         "sin" -> Func Sin e
         "cos" -> Func Cos e
 
 
+-- Function to parse a string into an 'Expr'
 readExpr :: String -> Maybe Expr
 readExpr input = case parse expr input of
   Just (expr, "") -> Just expr
   _               -> Nothing
 
--------------------------------------------------------------------
-
--- E --------------------------------------------------------------
-
+-- Function to generate arbitrary expressions for testing
 arbExpr :: Int -> Gen Expr
 arbExpr 0 = oneof [return X, Num <$> choose(0.0,100.0)]
 arbExpr n | n > 0 = oneof [ Num <$> choose(0.0,100.0)
@@ -154,6 +128,7 @@ arbExpr n | n > 0 = oneof [ Num <$> choose(0.0,100.0)
 instance Arbitrary Expr where
   arbitrary = sized arbExpr
 
+-- Function to identify associativity in nested expressions
 assoc :: Expr -> Expr
 assoc (Num n)                            = Num n
 assoc X                                  = X
@@ -163,20 +138,22 @@ assoc (AddMul op e1 e2)                  = AddMul op (assoc e1) (assoc e2)
 assoc (Func op e1)                       = Func op (assoc e1)
 
 
+-- Property to test conversion between 'Expr' and its string representation
 prop_ShowReadExpr :: Expr -> Bool
 prop_ShowReadExpr expr = case readExpr (showExpr expr) of
   Just expr' -> assoc expr == assoc expr'
   Nothing    -> False
 
------------------------------------------------------------------
 
--- smart adder :3
+-- Simplification functions for addition and multiplication
 add' (Num n) (Num m) = Num (n+m)
 add' (Num 0) e       = e
 add' e       (Num 0) = e
 add' e1      e2      = AddMul Add e1 e2
 
---smart mul
+func' :: Expr -> Expr
+func' (Func op (Num n)) = Num (eval (Func op (Num n)) 1)
+func' (Func op e)       = Func op e
 
 mul' (Num n) (Num m) = Num (n*m)
 mul' (Num 0) _       = Num 0
@@ -185,19 +162,19 @@ mul' (Num 1) e       = e
 mul' e       (Num 1) = e
 mul' e1      e2      = AddMul Mul e1 e2
 
-
+-- Function to simplify an expression
 simplify :: Expr -> Expr
 simplify (AddMul Add e1 e2) = add' (simplify e1) (simplify e2)
 simplify (AddMul Mul e1 e2) = mul' (simplify e1) (simplify e2)
-simplify (Func op e)        = Func op (simplify e)
+simplify (Func op e)        = func' $ Func op (simplify e)
 simplify X                  = X
 simplify (Num n)            = Num n
 
+-- Property to test if simplification preserves the value of an expression
 prop_SimplifyPreservesValue :: Expr -> Double -> Bool
 prop_SimplifyPreservesValue expr val = eval expr val == eval (simplify expr) val
 
--- F ------------------------------------------------------------
-
+-- Function to differentiate an expression
 differentiate :: Expr -> Expr
 differentiate (Num _)        = Num 0
 differentiate X              = Num 1
@@ -206,5 +183,5 @@ differentiate (AddMul Mul e1 e2) = simplify (AddMul Add (AddMul Mul (differentia
 differentiate (Func Sin e)   = simplify (AddMul Mul (differentiate e) (Func Cos e))
 differentiate (Func Cos e)   = simplify (AddMul Mul (Num (-1)) (AddMul Mul (differentiate e) (Func Sin e)))
 
--- PART 2 -------------------------------------------------------------
+
 
